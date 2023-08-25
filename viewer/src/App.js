b78362dde1bd8a1d4e43fe16b991acac635d649f
seotaro <mail2017@seotaro.com>
2023-08-26 00:10:49 +0900

    タイルの切り替え

--- viewer/src/App.js
+++ viewer/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 
 import Map from './components/map.js';
+import SettingsDrawer from './components/SettingsDrawer.js';
 
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(true);
+  const [isShowPmtilesLayer, showPmtilesLayer] = useState(true);
+  const [isShowXyzTileLayer, showXyzTileLayer] = useState(false);
+  const [isShowCogLayer, showCogLayer] = useState(false);
+
+  const onChangePmtilesLayer = (isShow) => {
+    showPmtilesLayer(isShow);
+  }
+  const onChagneXyzTileLayer = (isShow) => {
+    showXyzTileLayer(isShow);
+  }
+  const onChagneCogLayer = (isShow) => {
+    showCogLayer(isShow);
+  }
+
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
   return (
     <Box>
-      <Map />
+      <Map
+        isShowPmtilesLayer={isShowPmtilesLayer}
+        isShowXyzTileLayer={isShowXyzTileLayer}
+        isShowCogLayer={isShowCogLayer}
+      />
+
+      <SettingsDrawer
+        open={drawerOpen}
+        handleClose={handleDrawerClose}
+        onChangePmtilesLayer={onChangePmtilesLayer}
+        onChagneXyzTileLayer={onChagneXyzTileLayer}
+        onChagneCogLayer={onChagneCogLayer}
+      />
     </Box >
   );
 }
--- viewer/src/components/map.js
+++ viewer/src/components/map.js
@@ -10,11 +10,33 @@ const protocol = new pmtiles.Protocol();
 maplibregl.addProtocol('pmtiles', protocol.tile);
 
 export default function Map(props) {
+  const {
+    isShowPmtilesLayer,
+    isShowXyzTileLayer,
+    isShowCogLayer,
+  } = props;
+
+
   const mapContainer = useRef(null);
   const [lng] = useState(139.8);
   const [lat] = useState(35.3);
   const [zoom] = useState(2);
   const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    if (map == null) return;
+
+    map.setLayoutProperty('pmtiles-layer',
+      'visibility', isShowPmtilesLayer ? 'visible' : 'none');
+
+    map.setLayoutProperty('xyz-tile-layer',
+      'visibility', isShowXyzTileLayer ? 'visible' : 'none');
+
+    map.setLayoutProperty('cog-layer',
+      'visibility', isShowCogLayer ? 'visible' : 'none');
+
+  }, [isShowPmtilesLayer, isShowXyzTileLayer, isShowCogLayer]);
+
 
   useEffect(() => {
     const map = new maplibregl.Map({
@@ -35,38 +57,44 @@ export default function Map(props) {
         protocol.add(new pmtiles.PMTiles(URL));
 
         map.addLayer({
-          id: 'pmtiles',
+          id: 'pmtiles-layer',
           type: 'raster',
           source: {
             type: 'raster',
             url: 'pmtiles://' + URL,
           },
-          minzoom: 0,
+          layout: {
+            visibility: 'visible',
+          },
           maxzoom: 4,
         });
       }
 
       // ラスタータイル
       map.addLayer({
-        id: 'raster-tile',
+        id: 'xyz-tile-layer',
         type: 'raster',
         source: {
           type: 'raster',
           tileSize: 256,
           tiles: ['https://storage.googleapis.com/2023-08-25_map-tile/raster-tile/{z}/{x}/{y}.png'],
         },
-        minzoom: 0,
+        layout: {
+          visibility: 'none',
+        },
         maxzoom: 4,
       });
 
       // Cloud Optimized GeoTiff
       map.addLayer({
-        id: 'cogLayer',
+        id: 'cog-layer',
         type: 'raster',
         source: await generateCogSource(
           'https://storage.googleapis.com/2023-08-25_map-tile/cloud-optimized-geotiff.tiff'
         ),
-        minzoom: 0,
+        layout: {
+          visibility: 'none',
+        },
         maxzoom: 4,
       });
 
